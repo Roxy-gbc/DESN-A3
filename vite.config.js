df5494eebab6d1cc06a8e886cde1d56606f3d5f8
Frank Shen <101559709@georgebrown.ca>
2024-12-10 16:47:49 -0500

    fix CI CD

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -6,5 +7,15 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './vitest.setup.js',
+    coverage: {
+      provider: 'v8', // Use 'v8' as the coverage provider
+      reporter: ['text', 'json', 'html'],
+      include: ['src/**/*.{js,jsx,ts,tsx}'],
+    },
   },
 });
